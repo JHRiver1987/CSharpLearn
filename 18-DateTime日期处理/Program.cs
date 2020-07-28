bf6e8875b8dc557c0b2ea8f71c7d06b35d3dd71e
JHRiver1987 <jiangheriver@163.com>
2020-07-28 08:19:10 +0800

    增加了DateTime和TimeSpan类

---
+++ 18-DateTime日期处理/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _18_DateTime日期处理
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string s = "    ";
+            Console.WriteLine("请输入年份：");
+            int nYear = int.Parse(Console.ReadLine());
+            Console.WriteLine("请输入月份：");
+            int nMonth = int.Parse(Console.ReadLine());
+            DateTime day1 = new DateTime(nYear,nMonth,1);
+
+            int week1 = (int)day1.DayOfWeek;                        //获取该月第一天是星期几
+            int lastDay = day1.AddMonths(1).AddDays(-1).Day;        //获取该月最后一天
+
+            Console.WriteLine("{0}/{1}",day1.Year,day1.Month);
+            Console.WriteLine("日  一  二  三  四  五  六");
+            for(int i = 0; i < week1; i++)
+            {
+                Console.Write(s);
+            }
+            for(int i = 1; i <= lastDay; i++)
+            {
+                Console.Write("{0:00}  ",i);      //按01  02格式输出
+                if((i + week1) % 7 == 0)
+                {
+                    Console.WriteLine();
+                }
+            }
+
+            Console.WriteLine();
+            Console.WriteLine("按任意键退出...");
+            Console.ReadKey();
+        }
+    }
+}
---
+++ 19-TimeSpan时间处理/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace _19_TimeSpan时间处理
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            try
+            {
+                DateTime timeEnd = DateTime.Now.AddSeconds(60);     //在当前时间基础上加60秒
+                ThreadPool.QueueUserWorkItem((arg)=> {
+                    TimeSpan timeSpan = timeEnd - DateTime.Now;
+                    while (true)
+                    {
+                        Thread.Sleep(1000);
+                        if(timeSpan.TotalSeconds >= 0)
+                        {
+                            Console.WriteLine("还剩余{0}分钟{1}秒",timeSpan.Minutes,timeSpan.Seconds);
+                            timeSpan = timeSpan.Add(new TimeSpan(0,0,-1));
+                        }
+                    }
+                });
+            }
+            catch(Exception e)
+            {
+                Console.WriteLine(e.Message);
+            }
+            finally
+            {
+                Console.ReadLine();
+            }
+        }
+    }
+}
