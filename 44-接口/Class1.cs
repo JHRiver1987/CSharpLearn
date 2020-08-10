298336ee3bf6aa48db4369a7e3078ef3ecbdd492
River J <jiangheriver@163.com>
2020-08-10 22:17:45 +0800

    接口

---
+++ 44-接口/Class1.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _44_接口
+{
+    class Implementer : IMyInterface1, IMyInterface2
+    {
+        public void Method1()
+        {
+            Console.WriteLine("调用了Method-1。");
+        }
+
+        public void Method2()
+        {
+            Console.WriteLine("调用了Method-2。");
+        }
+    }
+}
---
+++ 44-接口/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _44_接口
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Implementer implementer = new Implementer();
+            implementer.Method1();
+            implementer.Method2();
+            Console.ReadKey();
+        }
+    }
+}
